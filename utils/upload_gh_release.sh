30669a664d0973870766a1d6e5e3ea6188d7ff03
camille-s <camille.seaberry@gmail.com>
2023-12-08 15:36:28 -0500

    Update upload script

--- utils/upload_gh_release.sh
+++ utils/upload_gh_release.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 YR=2022
 # metadata for other projects
+# is there already a release called dist? if not, create one
+if ! gh release view dist; then
+  gh release create dist --title "acs basic profile data"
+fi
 gh release upload dist "output_data/acs_town_basic_profile_${YR}.rds" --clobber
